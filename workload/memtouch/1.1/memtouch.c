93c1916c00bc64881e71aec31fc439bbbf0709f2
david78k <root@c11node6>
2014-06-15 01:01:35 -0400

    all

--- workload/memtouch/1.1/memtouch.c
+++ workload/memtouch/1.1/memtouch.c
@@ -1,8 +1,8 @@
 /* 
- * memtouch 2.0
+ * memtouch 1.1
  * 
- * Author: Tae Seung Kang 
- * Date: 6/15/2014
+ * by Tae Seung Kang 
+ * 6/14/2014
  *
  * usage: memtouch [size (mbytes)] [speed (mbytes/s)]
  *
@@ -15,7 +15,8 @@
  * the -no-dedupe option of Qemu.
  *
  * change log:
- * - get the memory information using sysinfo: memory used, total memory, free memory
+ * - input arugment speed 0 is acceptible
+ * - debugging mode is available by DEBUG constant
  * 
  */
 #include <string.h>
@@ -31,49 +32,8 @@
 #include <stdio.h>
 
 #include <limits.h>
-#include <sys/sysinfo.h>
 
 #define DEBUG 0
-//#define UNIT 1 //bytes (default)
-//#define UNIT 1024 // kbytes
-#define UNIT 1048576 // Mbytes 1024*1024
-
-// return memory to be allocated 
-//int getMemAlloc (char **argv) {
-int getMemAlloc (int mem_size) {
-       // total memory allocated in MB
-        int mem_alloc = 400;
-   //     int mem_alloc_bytes = 0;
-  //      double *d;
-
-        int mem_total = 0;
-        int mem_free = 0;
-        int mem_used = 0;
-
-        struct sysinfo si;
-
-        if(sysinfo(&si) < 0) {
-                printf("error! couldn't get sin");
-        }
-
-        mem_total = si.totalram/UNIT;
-        mem_free = si.freeram/UNIT;
-        mem_used = mem_total - mem_free;
-        printf ("mem total: %d\n", mem_total);
-        printf ("mem free: %d\n", mem_free);
-        printf ("mem used: %d\n", mem_used);
-
-        //mem_alloc = atoi (argv[1]);
-        mem_alloc = mem_size - mem_used;
-    //    mem_alloc_bytes = mem_alloc * 1024 * 1024;
-
-//        d = malloc (mem_alloc_bytes);
-        printf ("mem allocated: %d\n", mem_alloc);
-
-        if (mem_alloc <= 0) exit(1);
-
-	return mem_alloc;
-}
 
 int main(int argc, char **argv)
 {
@@ -87,14 +47,12 @@ int main(int argc, char **argv)
 	int i;
 
 	if (argc != 3) {
-		//printf ("usage: %s [size (mbytes)] [speed (mbytes/s)]\n", argv[0]);
-		printf ("usage: memtouch [size (mbytes)] [speed (mbytes/s)]\n", argv[0]);
+		printf ("usage: %s [size (mbytes)] [speed (mbytes/s)]\n", argv[0]);
 		return 1;
 	}
 
 	/* ./a.out [size (mbytes)] [speed (mbytes/s)] */
-	//unsigned long memsize = atoi(argv[1]) * 1024 * 1024;
-	unsigned long memsize = getMemAlloc(atoi(argv[1])) * 1024 * 1024;
+	unsigned long memsize = atoi(argv[1]) * 1024 * 1024;
 
 	fd = open("random.dat", O_RDONLY);
 	if (fd < 0) {
